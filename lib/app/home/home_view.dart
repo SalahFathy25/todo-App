7d5251ef1246d6b4a16a578aae1adf0966799d79
Salah Fathy <SalahFathy@gmail.com>
2024-09-21 21:32:05 +0300

    todo App

--- lib/app/home/home_view.dart
+++ lib/app/home/home_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:to_do_list_app/app/data/hive_data_storage.dart';
 import 'package:to_do_list_app/app/home/cubit/cubit/home_cubit.dart';
 
+import '../../main.dart';
 import 'widgets/FAB.dart';
 import 'widgets/build_body.dart';
 
@@ -13,12 +14,24 @@ class HomeView extends StatefulWidget {
   State<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView> {
+class _HomeViewState extends State<HomeView> with RouteAware {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    routeObserver.subscribe(this, ModalRoute.of(context)! as PageRoute);
+  }
+
+  @override
+  void dispose() {
+    routeObserver.unsubscribe(this);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => HomeCubit(HiveDataStorage())..loadTasks(),
-      child:const SafeArea(
+      child: const SafeArea(
         child: Scaffold(
           backgroundColor: Colors.white,
           floatingActionButton: Fab(),
